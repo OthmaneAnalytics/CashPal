f1ec637f525eb464269453efa2425f9cd04c8c63
marbel <othmane.xx90@gmail.com>
2024-12-22 21:48:59 +0100

    creating table

--- up.sql
+++ up.sql
@@ -2,4 +2,12 @@ CREATE TABLE users (id INTEGER, name TEXT, age INTEGER, balance INTEGER, is_admi
 INSERT into users (id, name, age, balance, is_admin) values (1, 'John Smith', 28, 450.00, true);
 INSERT into users (id, name, age, balance, is_admin) values (2, 'Darren Walker', 27, 200.00, true);
 INSERT into users (id, name, age, balance, is_admin) values (3, 'Jane Morris', 33, 496.24, false);
+CREATE TABLE people(
+	id INTEGER,
+	tag TEXT,
+	name TEXT,
+	age INTEGER,
+	balance INTEGER
+	is_admin BOOLEAN
+)
 
