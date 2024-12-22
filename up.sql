3e3e01b6695c9c61010597d9fa25580eefe63ff5
marbel <othmane.xx90@gmail.com>
2024-12-22 21:25:33 +0100

    sql select statement

--- up.sql
+++ up.sql
@@ -1,4 +1,5 @@
-CREATE TABLE users (id INTEGER, name TEXT, age INTEGER, is_admin BOOLEAN);
-INSERT into users (id, name, age, is_admin) values (1, 'John Doe', 27, false);
-INSERT into users (id, name, age, is_admin) values (2, 'Sally Rae', 18, true);
+CREATE TABLE users (id INTEGER, name TEXT, age INTEGER, balance INTEGER, is_admin BOOLEAN);
+INSERT into users (id, name, age, balance, is_admin) values (1, 'John Smith', 28, 450.00, true);
+INSERT into users (id, name, age, balance, is_admin) values (2, 'Darren Walker', 27, 200.00, true);
+INSERT into users (id, name, age, balance, is_admin) values (3, 'Jane Morris', 33, 496.24, false);
 
