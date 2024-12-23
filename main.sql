cf1e2b31a40bbe68605d038b77dd911556141cf1
marbel <othmane.xx90@gmail.com>
2024-12-23 15:34:00 +0100

    %

--- main.sql
+++ main.sql
@@ -5,4 +5,4 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-SELECT name, age, country_code FROM users WHERE country_code IN ('CA', 'US', 'MX');
+SELECT * FROM users WHERE name LIKE 'Al%';
