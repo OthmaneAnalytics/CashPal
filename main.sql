894b13e01697016d6c0092a716c26a64bdb09678
marbel <othmane.xx90@gmail.com>
2024-12-23 16:26:23 +0100

    limit

--- main.sql
+++ main.sql
@@ -5,4 +5,6 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-SELECT * FROM users WHERE name LIKE 'Al___';
+SELECT * FROM transactions
+WHERE note LIKE '%lunch%' 
+LIMIT 5;
