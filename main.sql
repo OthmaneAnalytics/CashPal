23c4827703442ca2865e40bc60c08a31c488edab
marbel <othmane.xx90@gmail.com>
2024-12-23 12:16:25 +0100

    where

--- main.sql
+++ main.sql
@@ -5,11 +5,4 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-INSERT INTO users(name, age, country_code, username, password, is_admin)
-VALUES('Lance', 20, 'US', 'LanChr', 'bootdevisbest', false);
-INSERT INTO users(name, age, country_code, username, password, is_admin)
-VALUES('Tiffany', 28, 'US', 'Tifferoon', 'autoincrement', true);
-
-
-
-SELECT * FROM users;
+SELECT username FROM users WHERE is_admin = true;
