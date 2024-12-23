263df2506d4ec07428a70cd737aea9ebc94ef3a6
marbel <othmane.xx90@gmail.com>
2024-12-23 11:34:25 +0100

    crud

--- main.sql
+++ main.sql
@@ -5,14 +5,4 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-CREATE TABLE transactions(
-    id INTEGER PRIMARY KEY,
-    sender_id INTEGER,
-    recipient_id INTEGER,
-    memo TEXT NOT NULL,
-    amount  INTEGER NOT NULL,
-    balance INTEGER NOT NULL
-);
-
-PRAGMA TABLE_INFO('transactions');
-
+SELECT * FROM crud; 
--- up.sql
+++ up.sql
@@ -1,32 +1,4 @@
-PRAGMA foreign_keys = ON;
-
-CREATE table countries(code TEXT PRIMARY KEY, name TEXT);
-
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER,
-    country_code TEXT NOT NULL,
-    username TEXT ,
-    password TEXT,
-    is_admin BOOLEAN,
-    FOREIGN KEY (country_code)
-    REFERENCES countries(code)
-);
-
-INSERT into countries (
-    code,
-    name
-) values (
-    'US',
-    'United States of America'
-);
-
-INSERT into countries (
-    code,
-    name
-) values (
-    'IN',
-    'India'
-);
+CREATE TABLE crud (c TEXT, r TEXT, u TEXT, d TEXT);
+INSERT INTO crud (c, r, u, d)
+VALUES ('CREATE = CREATE', 'READ = SELECT', 'UPDATE = UPDATE', 'DELETE = DELETE');
 
