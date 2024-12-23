f1c00d1a92bafb343d2a724c1c4c4c7e45ddc756
marbel <othmane.xx90@gmail.com>
2024-12-23 15:31:19 +0100

    in

--- main.sql
+++ main.sql
@@ -5,4 +5,4 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-SELECT * FROM users WHERE (country_code = 'CA' OR country_code = 'US') AND age < 18;
+SELECT name, age, country_code FROM users WHERE country_code IN ('CA', 'US', 'MX');
