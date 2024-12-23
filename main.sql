d5f2f1870a7ce02c91d98c2c9cb4b1081d7bae73
marbel <othmane.xx90@gmail.com>
2024-12-23 12:56:50 +0100

    delete

--- main.sql
+++ main.sql
@@ -5,4 +5,7 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-SELECT * FROM transactions WHERE sender_id IS NOT NULL;
+DELETE FROM users
+    WHERE name = 'Samantha';
+SELECT * FROM users;
+
