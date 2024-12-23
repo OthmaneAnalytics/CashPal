c370f909fa3c54720c627a044cc7c662704b9fea
marbel <othmane.xx90@gmail.com>
2024-12-23 11:52:08 +0100

    auto increment

--- main.sql
+++ main.sql
@@ -5,10 +5,10 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-INSERT INTO users(id, name, age, country_code, username, password, is_admin)
-VALUES(1, 'David', 34, 'US', 'DavidDev', 'insertPractice', false);
-INSERT INTO users(id, name, age, country_code, username, password, is_admin)
-VALUES(2, 'Samantha', 29, 'BR', 'Sammy93', 'addingRecords!', false);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES('Lance', 20, 'US', 'LanChr', 'bootdevisbest', false);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES('Tiffany', 28, 'US', 'Tifferoon', 'autoincrement', true);
 
 
 
