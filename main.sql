995ce332a32d98f5b590022e1d88d188bf4a2a05
marbel <othmane.xx90@gmail.com>
2024-12-23 13:01:21 +0100

    updata

--- main.sql
+++ main.sql
@@ -5,7 +5,11 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-DELETE FROM users
-    WHERE name = 'Samantha';
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Lane', 27, 'US', 'wagslane', 'update_me', false);
+UPDATE users
+SET is_admin = true
+WHERE name = 'Lane';
+
 SELECT * FROM users;
 
