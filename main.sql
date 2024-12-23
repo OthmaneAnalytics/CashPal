46869306097245f61aa31f88726247863f0060dd
marbel <othmane.xx90@gmail.com>
2024-12-23 15:21:33 +0100

    distinct

--- main.sql
+++ main.sql
@@ -5,4 +5,4 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-SELECT name, age FROM users WHERE age BETWEEN 18 AND 30;
+SELECT DISTINCT country_code FROM users;
