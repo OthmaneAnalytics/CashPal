39478ad31723ba1627ce938376149e00c7117141
marbel <othmane.xx90@gmail.com>
2024-12-23 10:48:18 +0100

    primary key

--- main.sql
+++ main.sql
@@ -5,6 +5,41 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
+INSERT into users (
+    id,
+    name,
+    age,
+    username,
+    password,
+    is_admin
+) values (
+    1,
+    'Rudolf',
+    33,
+    'rudolf1234',
+    'thisisnotsecure',
+    false
+);
+
+INSERT into users (
+    id,
+    name,
+    age,
+    username,
+    password,
+    is_admin
+) values (
+    1,
+    'Jerry',
+    25,
+    'jerrysmith',
+    'mypasswordis1234',
+    true
+);
+
+SELECT * FROM users;
 
 
-SELECT * FROM transactions;
+PRAGMA TABLE_INFO('users');
+
+
--- up.sql
+++ up.sql
@@ -1,23 +1,8 @@
-CREATE TABLE transactions (
-    id INTEGER, 
-    recipient_id INTEGER, 
-    sender_id INTEGER, 
-    note TEXT, 
-    amount INTEGER
-);
-
-INSERT INTO transactions (id, recipient_id, sender_id, note, amount)
-VALUES (1, 14, 26, 'Testing transaction!', 10.50);
-
-INSERT INTO transactions (id, sender_id)
-VALUES (2, 4);
-
-INSERT INTO transactions (recipient_id, note, amount)
-VALUES (5, 'Oil change, full synthetic', 140.22);
-
-ALTER TABLE transactions
-ADD COLUMN transaction_type TEXT;
-
-ALTER TABLE transactions
-ADD COLUMN was_successful BOOLEAN;
-
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    contry_code TEXT UNIQUE,
+    password TEXT NOT NULL,
+    is_admin BOOLEAN
+)
