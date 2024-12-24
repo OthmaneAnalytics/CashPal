b8eb87fbbbed035a2c923064721326e3c96a3be7
marbel <othmane.xx90@gmail.com>
2024-12-24 07:49:57 +0100

    min group by average

--- main.sql
+++ main.sql
@@ -5,5 +5,7 @@
 
 -- Optional: Adjust column widths (for SQLite CLI)
 --.width 20 30 10  -- Adjust these numbers as needed
-SELECT user_id ,max(amount) FROM transactions
-WHERE user_id = 4;
+--SELECT min(age) FROM users WHERE country_code = 'US';
+
+--SELECT user_id, sum(amount) FROM transactions WHERE was_successful = true GROUP BY user_id;
+SELECT avg(age) FROM users WHERE country_code = 'US';
--- up.sql
+++ up.sql
@@ -9,31 +9,31 @@ CREATE TABLE transactions (
 );
 
 INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (9, 4, 'Testing transaction!', 10.50, true);
+VALUES (1, 4, 'Testing transaction!', 10.50, true);
 
 INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (5, 10, 'Thanks for lunch!', -9.56, true);
+VALUES (2, 10, 'Thanks for lunch!', -9.56, true);
 
 INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (6, 2, 'Car problems', 256.21, false);
+VALUES (2, 2, 'Car problems', 256.21, false);
 
 INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (4, 8, 'Happy birthday!!', -50, true);
+VALUES (2, 8, 'Happy birthday!!', -50, true);
 
 INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (4, 11, 'MTG Draft', 50, false);
+VALUES (3, 11, 'MTG Draft', 50, false);
 
 INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (4, 4, 'lunch with the friends', -12.56, true);
+VALUES (3, 4, 'lunch with the friends', -12.56, true);
 
 INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (4, 12, 'paying ya back for lunch', 12.22, false);
+VALUES (3, 12, 'paying ya back for lunch', 12.22, false);
 
 INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (9, 6, 'lunch break', -24.89, true);
+VALUES (4, 6, 'lunch break', -24.89, true);
 
 INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (1, 23, 'thanks for lunch yesterday', 10.00, true);
+VALUES (4, 23, 'thanks for lunch yesterday', 10.00, true);
 
 INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
 VALUES (4, 14, '5 buck pizza for lunch', -5.00, true);
@@ -42,14 +42,11 @@ INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
 VALUES (4, 2, 'lunch was goooood thanks, man!', 47.42, true);
 
 INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (23, 4, 'lunch meetup, lets get together again soon.', -16.91, false);
+VALUES (5, 4, 'lunch meetup, lets get together again soon.', -16.91, false);
 
 INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
 VALUES (6, 14, 'not sure how much lunch was, heres 20', 20.00, true);
 
 INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (2, 13, 'Happy birthday, sis! Lets get lunch soon.', -100.00, true);
-
-INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (4, 2, 'babysitting fee', 55.00, true);
+VALUES (6, 13, 'Happy birthday, sis! Lets get lunch soon.', -100.00, true);
 
