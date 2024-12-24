96851e70d4dd21a4437079973d4bf299e2d48cdd
marbel <othmane.xx90@gmail.com>
2024-12-24 08:17:31 +0100

    round

--- main.sql
+++ main.sql
@@ -9,9 +9,8 @@
 
 --SELECT user_id, sum(amount) FROM transactions WHERE was_successful = true GROUP BY user_id;
 --SELECT avg(age) FROM users WHERE country_code = 'US';
-SELECT sender_id, SUM(amount) AS balance
-FROM transactions
-WHERE sender_id IS NOT NULL AND note LIKE '%lunch%'
-GROUP BY sender_id
-HAVING balance > 20 
-ORDER BY balance;
+--SELECT sender_id, SUM(amount) AS balance FROM transactions WHERE sender_id IS NOT NULL AND note LIKE '%lunch% GROUP BY sender_id HAVING balance > 20 ORDER BY balance;
+SELECT round(avg(age)) AS round_age
+FROM users
+WHERE country_code = 'US';
+
--- up.sql
+++ up.sql
@@ -1,58 +1,49 @@
-CREATE TABLE transactions (
-  id INTEGER PRIMARY KEY, 
-  user_id INTEGER NOT NULL,
-  recipient_id INTEGER, 
-  sender_id INTEGER, 
-  note TEXT, 
-  amount INTEGER,
-  was_successful BOOLEAN
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    country_code TEXT NOT NULL,
+    username TEXT UNIQUE,
+    password TEXT NOT NULL,
+    is_admin BOOLEAN
 );
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (1, 2, 'Testing lunch transaction!', 10.50, true);
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (1, 'David', 34, 'US', 'DavidDev', 'insertPractice', false);
 
-INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (2, 10, 'Thanks for lunch!', -9.56, true);
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (2, 'Samantha', 29, 'BR', 'Sammy93', 'addingRecords!', false);
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (2, 2, 'Car lunch problems', 256.21, false);
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (3, 'John', 39, 'CA', 'Jjdev21', 'welovebootdev', false);
 
-INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (2, 8, 'Happy birthday!!', -50, true);
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (4, 'Ram', 42, 'IN', 'Ram11c', 'thisSQLcourserocks', false);
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (3, 3, 'MTG Draft', 50, false);
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (5, 'Hunter', 30, 'US', 'Hdev92', 'backendDev', false);
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (3, 3, 'a lunch with the friends', -12.56, true);
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (6, 'Allan', 27, 'US', 'Alires', 'iLoveB00tdev', true);
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (3, 3, 'paying ya back for lunch', 12.22, false);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Lance', 20, 'US', 'LanChr', 'b00tdevisbest', false);
 
-INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (4, 6, 'lunch break', -24.89, true);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Tiffany', 28, 'US', 'Tifferoon', 'autoincrement', true);
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful) 
-VALUES (4, 4, 'thanks for lunch yesterday', 10.00, true);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Lane', 27, 'US', 'wagslane', 'update_me', false);
 
-INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (4, 14, '5 buck pizza for lunch', -5.00, true);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Darren', 15, 'CA', 'Dshan', 'found_me', false);
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (4, 4, 'lunch was goooood thanks, man!', 47.42, true);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Albert', 55, 'BR', 'BertDev', 'one_al_name', false);
 
-INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (5, 4, 'lunch meetup, lets get together again soon.', -16.91, false);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Alvin', 27, 'US', 'AlvinA27', 'easter_egg', false);
 
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (6, 4, 'not sure how much lunch was, heres 20', 20.00, true);
-
-INSERT INTO transactions (user_id, recipient_id, note, amount, was_successful)
-VALUES (6, 13, 'Happy birthday, sis! Lets get lunch soon.', -100.00, true);
-
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (5, 9, 'Payin ya back for lunch on friday', 13.21, true);
-
-INSERT INTO transactions (user_id, sender_id, note, amount, was_successful)
-VALUES (5, 5, 'Lunch break with the boys', 22.40, true);
+INSERT INTO users(name, age, country_code, username, password, is_admin)
+VALUES ('Al', 39, 'JP', 'quickCoder', 'snake_case', false);
 
